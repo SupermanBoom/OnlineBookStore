3ffc5aab7f2f778881c14406b2d2d00adb2046e7
supermanboom78 <supermanboom78@gmail.com>
2026-03-24 22:05:44 +0800

    exp2

--- bookshop/src/com/shine/bookshop/dao/SiteInfoDao.java
+++
@@ -1,9 +0,0 @@
-package com.shine.bookshop.dao;
-
-import com.shine.bookshop.bean.SiteInfo;
-
-public interface SiteInfoDao {
-	SiteInfo getSiteInfo();
-
-	boolean saveSiteInfo(SiteInfo siteInfo);
-}
